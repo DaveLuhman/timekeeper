f4ce3d175af327a8e05f65dc66aee12b05962967
Dave Luhman <dave@ado.software>
2026-05-31 05:24:22 -0500

    Remove automation scripts and related utilities for email notifications and report generation. This includes the deletion of cron jobs, aggregation functions, and daily entry processing, streamlining the automation module.

--- automation/index.js
+++
@@ -1,9 +0,0 @@
-import aggregateReport from './aggregate/index.js'
-import testEmail from './testEmail/index.js'
-import dailyEntries from './dailyEntries/index.js'
-/**
- * Array of scheduled tasks.
- * @type {Array<Function>}
- */
-const scheduledTasks = [aggregateReport, testEmail, dailyEntries]
-export default scheduledTasks
